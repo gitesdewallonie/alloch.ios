c92c4410bee50b54db714b95bbb3012ed51e75ad
Laurent Lasudry <lasudry@gmail.com>
2015-04-01 12:13:25 +0200

    Simplify platforms

--- AlloChambre/Helper Classes/UIDeviceHardware.m
+++ AlloChambre/Helper Classes/UIDeviceHardware.m
@@ -25,37 +25,45 @@
     if ([platform isEqualToString:@"iPhone1,2"])    return @"iPhone 3G";
     if ([platform isEqualToString:@"iPhone2,1"])    return @"iPhone 3GS";
     if ([platform isEqualToString:@"iPhone3,1"])    return @"iPhone 4";
-    if ([platform isEqualToString:@"iPhone3,3"])    return @"Verizon iPhone 4";
+    if ([platform isEqualToString:@"iPhone3,3"])    return @"iPhone 4";
     if ([platform isEqualToString:@"iPhone4,1"])    return @"iPhone 4S";
-    if ([platform isEqualToString:@"iPhone5,1"])    return @"iPhone 5 (GSM)";
-    if ([platform isEqualToString:@"iPhone5,2"])    return @"iPhone 5 (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPhone5,3"])    return @"iPhone 5c (GSM)";
-    if ([platform isEqualToString:@"iPhone5,4"])    return @"iPhone 5c (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPhone6,1"])    return @"iPhone 5s (GSM)";
-    if ([platform isEqualToString:@"iPhone6,2"])    return @"iPhone 5s (GSM+CDMA)";
+    if ([platform isEqualToString:@"iPhone5,1"])    return @"iPhone 5";
+    if ([platform isEqualToString:@"iPhone5,2"])    return @"iPhone 5";
+    if ([platform isEqualToString:@"iPhone5,3"])    return @"iPhone 5c";
+    if ([platform isEqualToString:@"iPhone5,4"])    return @"iPhone 5c";
+    if ([platform isEqualToString:@"iPhone6,1"])    return @"iPhone 5s";
+    if ([platform isEqualToString:@"iPhone6,2"])    return @"iPhone 5s";
+    if ([platform isEqualToString:@"iPhone7,1"])    return @"iPhone 6 Plus";
+    if ([platform isEqualToString:@"iPhone7,2"])    return @"iPhone 6";
     if ([platform isEqualToString:@"iPod1,1"])      return @"iPod Touch 1G";
     if ([platform isEqualToString:@"iPod2,1"])      return @"iPod Touch 2G";
     if ([platform isEqualToString:@"iPod3,1"])      return @"iPod Touch 3G";
     if ([platform isEqualToString:@"iPod4,1"])      return @"iPod Touch 4G";
     if ([platform isEqualToString:@"iPod5,1"])      return @"iPod Touch 5G";
     if ([platform isEqualToString:@"iPad1,1"])      return @"iPad";
-    if ([platform isEqualToString:@"iPad2,1"])      return @"iPad 2 (WiFi)";
-    if ([platform isEqualToString:@"iPad2,2"])      return @"iPad 2 (GSM)";
-    if ([platform isEqualToString:@"iPad2,3"])      return @"iPad 2 (CDMA)";
-    if ([platform isEqualToString:@"iPad2,4"])      return @"iPad 2 (WiFi)";
-    if ([platform isEqualToString:@"iPad2,5"])      return @"iPad Mini (WiFi)";
-    if ([platform isEqualToString:@"iPad2,6"])      return @"iPad Mini (GSM)";
-    if ([platform isEqualToString:@"iPad2,7"])      return @"iPad Mini (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPad3,1"])      return @"iPad 3 (WiFi)";
-    if ([platform isEqualToString:@"iPad3,2"])      return @"iPad 3 (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPad3,3"])      return @"iPad 3 (GSM)";
-    if ([platform isEqualToString:@"iPad3,4"])      return @"iPad 4 (WiFi)";
-    if ([platform isEqualToString:@"iPad3,5"])      return @"iPad 4 (GSM)";
-    if ([platform isEqualToString:@"iPad3,6"])      return @"iPad 4 (GSM+CDMA)";
-    if ([platform isEqualToString:@"iPad4,1"])      return @"iPad Air (WiFi)";
-    if ([platform isEqualToString:@"iPad4,2"])      return @"iPad Air (Cellular)";
-    if ([platform isEqualToString:@"iPad4,4"])      return @"iPad mini 2G (WiFi)";
-    if ([platform isEqualToString:@"iPad4,5"])      return @"iPad mini 2G (Cellular)";
+    if ([platform isEqualToString:@"iPad2,1"])      return @"iPad 2";
+    if ([platform isEqualToString:@"iPad2,2"])      return @"iPad 2";
+    if ([platform isEqualToString:@"iPad2,3"])      return @"iPad 2";
+    if ([platform isEqualToString:@"iPad2,4"])      return @"iPad 2";
+    if ([platform isEqualToString:@"iPad2,5"])      return @"iPad Mini";
+    if ([platform isEqualToString:@"iPad2,6"])      return @"iPad Mini";
+    if ([platform isEqualToString:@"iPad2,7"])      return @"iPad Mini";
+    if ([platform isEqualToString:@"iPad3,1"])      return @"iPad 3";
+    if ([platform isEqualToString:@"iPad3,2"])      return @"iPad 3";
+    if ([platform isEqualToString:@"iPad3,3"])      return @"iPad 3";
+    if ([platform isEqualToString:@"iPad3,4"])      return @"iPad 4";
+    if ([platform isEqualToString:@"iPad3,5"])      return @"iPad 4";
+    if ([platform isEqualToString:@"iPad3,6"])      return @"iPad 4";
+    if ([platform isEqualToString:@"iPad4,1"])      return @"iPad Air";
+    if ([platform isEqualToString:@"iPad4,2"])      return @"iPad Air";
+    if ([platform isEqualToString:@"iPad4,3"])      return @"iPad Air";
+    if ([platform isEqualToString:@"iPad5,3"])      return @"iPad Air 2";
+    if ([platform isEqualToString:@"iPad5,4"])      return @"iPad Air 2";
+    if ([platform isEqualToString:@"iPad4,4"])      return @"iPad mini 2G";
+    if ([platform isEqualToString:@"iPad4,5"])      return @"iPad mini 2G";
+    if ([platform isEqualToString:@"iPad4,7"])      return @"iPad Mini 3";
+    if ([platform isEqualToString:@"iPad4,8"])      return @"iPad Mini 3";
+    if ([platform isEqualToString:@"iPad4,9"])      return @"iPad Mini 3";
     if ([platform isEqualToString:@"i386"])         return @"Simulator";
     if ([platform isEqualToString:@"x86_64"])       return @"Simulator";
     return platform;
@@ -66,23 +74,21 @@
     UIDeviceHardware *hw = [[UIDeviceHardware alloc] init];
     BOOL result = NO;
     
-    if ([[hw platformString] isEqualToString:@"iPhone 5 (GSM)"]
-        || [[hw platformString] isEqualToString:@"iPhone 5 (GSM+CDMA)"]
-        || [[hw platformString] isEqualToString:@"iPhone 5c (GSM)"]
-        || [[hw platformString] isEqualToString:@"iPhone 5c (GSM+CDMA)"]
-        || [[hw platformString] isEqualToString:@"iPhone 5s (GSM)"]
-        || [[hw platformString] isEqualToString:@"iPhone 5s (GSM+CDMA)"]
+    if ([[hw platformString] isEqualToString:@"iPhone 5"]
+        || [[hw platformString] isEqualToString:@"iPhone 5c"]
+        || [[hw platformString] isEqualToString:@"iPhone 5s"]
         || [[hw platformString] isEqualToString:@"iPod Touch 5G"]
         ) {
-        
         result = YES;
-    } else if([[hw platformString] isEqualToString:@"Simulator"]) {
+    }
+	else if([[hw platformString] isEqualToString:@"Simulator"]) {
         CGRect screenBounds = [[UIScreen mainScreen] bounds];
         if (screenBounds.size.height == 568) {
             // code for 4-inch screen
             result = YES;
         } else {
             // code for 3.5-inch screen
+			result = NO;
         }
     }
     
